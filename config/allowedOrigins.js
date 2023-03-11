1442b097c2a7ac3b83f5055b243b3dbbdd2cba5c
azharcool <azharuddinmullah219@gmail.com>
2023-03-11 15:19:53 +0530

    update origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://www.yoursite.com", "http://localhost:3500/", "http://localhost:3000/"];
+const allowedOrigins = ["https://www.yoursite.com", "http://localhost:3500/", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
